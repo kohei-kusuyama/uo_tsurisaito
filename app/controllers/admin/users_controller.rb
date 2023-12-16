9f979e9b59314141070a454045d47c643cfe44ed
KoheiKusuyama <144124225+kohei-kusuyama@users.noreply.github.com>
2023-12-16 05:09:30 +0000

    管理者のtopページ

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,11 +4,26 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "変更が完了しました"
+      redirect_to admin_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :nickname, :email, :status)
   end
 end
