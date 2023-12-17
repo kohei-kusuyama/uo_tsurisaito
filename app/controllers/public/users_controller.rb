540af6ea68dc37795e955fabd306c8df85c6f2aa
KoheiKusuyama <144124225+kohei-kusuyama@users.noreply.github.com>
2023-12-17 07:20:56 +0000

    いいね一覧ok

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -38,9 +38,8 @@ class Public::UsersController < ApplicationController
 
   def favorites
     @user = User.find(params[:id])
-    favorites = Favorite.where(user_id: @user).pluck(:post_id)
-    @favorite_posts = Post.find(favorites)
-    @post = Post.find(params[:id])
+    favorite_post_ids = Favorite.where(user_id: @user.id).pluck(:post_id)
+    @favorite_posts = Post.where(id: favorite_post_ids)
   end
 end
 
