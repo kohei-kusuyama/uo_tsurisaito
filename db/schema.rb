7eea13b20cfc85c2da67b5f971c2553414c20214
KoheiKusuyama <144124225+kohei-kusuyama@users.noreply.github.com>
2023-12-23 14:27:04 +0000

    google_map_API 地図表示ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_19_104946) do
+ActiveRecord::Schema.define(version: 2023_12_22_060137) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -85,6 +85,9 @@ ActiveRecord::Schema.define(version: 2023_12_19_104946) do
     t.string "size", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "relationships", force: :cascade do |t|
